1e2e29a4e88abff6b0c36c6cb0b0d2c5af496db1
Valentin <stamatevalentin125@gmail.com>
2020-07-14 21:49:27 +0300

    added routing

--- Web/client/src/App.tsx
+++ Web/client/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css';
+import SignUp from "./Components/Signup/Signup";
 
 function App() {
   return (
     <div className="App">
-        
+        <BrowserRouter>
+            <Route path="/signup" exact component={SignUp}/>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ Web/client/src/Components/Signup/Signup.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SignUp() {
+    return (
+        <div>
+            Ana are 2 mere rosii
+        </div>
+    );
+}
